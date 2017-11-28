e429705623a3457804663de4bce3086fc2dfd416
fvera87 <fernando.vl87@gmail.com>
2017-11-28 23:00:14 +0100

    things missing before

--- exercise2/generateMap.py
+++ exercise2/generateMap.py
@@ -29,7 +29,7 @@ for lat,lon, n, el in zip(lat, lon, name, elev):
 fg2 = folium.FeatureGroup("CountryPopulation")
 fg2.add_child(folium.GeoJson(data=open("world.json", "r", encoding="utf-8-sig").read(),
              style_function=lambda x: {'fillColor': 'green' if x['properties']['POP2005'] < 10000000
-else 'orange' if x['properties']['POP2005'] < 20000000 else 'red'}))
+                            else 'orange' if x['properties']['POP2005'] < 20000000 else 'red'}))
 
 map.add_child(fg)
 map.add_child(fg2)
